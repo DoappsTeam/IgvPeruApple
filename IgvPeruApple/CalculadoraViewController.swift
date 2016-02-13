c6732c1d3ab51355371c868fba78709cf29cbcd2
TOBY <bryanlazarotoby@gmail.com>
2016-02-13 14:26:07 -0500

    storyboard calculadora parte 4

--- IgvPeruApple/CalculadoraViewController.swift
+++ IgvPeruApple/CalculadoraViewController.swift
@@ -16,6 +16,19 @@ class CalculadoraViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
