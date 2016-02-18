b4caff8489b57d118b1781b6fddcb14068b2a747
jonathan <jnolascob@gmail.com>
2016-02-18 11:37:23 -0500

    update style app

--- IgvPeruApple/ViewController.swift
+++ IgvPeruApple/ViewController.swift
@@ -16,15 +16,30 @@ class ViewController: UIViewController {
              igvPageViewController?.pageDelegate = self
         }
     }
+    
+    // MARK : init
     override func viewDidLoad() {
         super.viewDidLoad()
+        customNavigation()
     }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let igvPVC = segue.destinationViewController as? IGVPageViewController {
             self.igvPageViewController = igvPVC
         }
     }
+    
+    // MARK : Custom Navigation Bar
+    func customNavigation(){
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        //self.navigationController?.navigationBar.barTintColor = UIColor(red: 25/255, green: 117/255, blue: 146/255, alpha: 1.0)
+        let imageNav:UIImage = UIImage(named: "button_background")!
+        self.navigationController?.navigationBar.setBackgroundImage(imageNav, forBarMetrics: .Default)
+        
+    }
 }
+
+
 extension ViewController: IGVPageViewControllerDelegate{
     func pageViewController(pageViewController: UIPageViewController, didUpdatePageIndex index: Int) {
         self.segmento.selectedSegmentIndex = index
