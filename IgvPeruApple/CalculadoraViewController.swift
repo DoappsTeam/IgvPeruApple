9f7714533d9717616eca2d9fc93798dd9fe37759
TOBY <bryanlazarotoby@gmail.com>
2016-02-18 11:01:24 -0500

    calculadora :( falta fuente y validación de 2 puntos)

--- IgvPeruApple/CalculadoraViewController.swift
+++ IgvPeruApple/CalculadoraViewController.swift
@@ -70,7 +70,7 @@ class CalculadoraViewController: UIViewController, UIPickerViewDelegate, UIPicke
                 textIGV.text = ""
                 textTotal.text = ""
                 }
-            
+            //as
             }
         else
             {
